c2b7cad5a0baea814f99ba9d294f89f601175653
Mathieu Dupuy <deronnax@gmail.com>
2023-02-27 11:36:19 +0100

    ADMINS

--- toogoodtogo/settings/production.py
+++ toogoodtogo/settings/production.py
@@ -17,3 +17,8 @@ DATABASES = {
 STATIC_ROOT = BASE_DIR / 'static'
 
 MIDDLEWARE.insert(1, 'whitenoise.middleware.WhiteNoiseMiddleware')
+
+# should be in environment variables, or even not here at all (use something like Sentry)
+ADMINS = [
+    ('Mathieu Dupuy', 'deronnax@gmail.com')
+]
